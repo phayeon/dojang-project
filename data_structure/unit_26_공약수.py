33e103bd0e11830963cf25ce1c9a432f4a255d7f
phayeon <phayeon1222@gmail.com>
2022-11-09 20:04:54 +0900

    11-09 20:04

--- data_structure/unit_26_공약수.py
+++ data_structure/unit_26_공약수.py
@@ -1,3 +1,9 @@
+"""
+공약수의 합 구하기
+두 숫자의 공약수의 합이 결과로 나오도록 해라.
+입력은 한번에 '10 20' 으로 받는다.
+"""
+
 def multiple():
     num = input().split()
     num1, num2 = int(num[0]), int(num[1])
@@ -7,7 +13,7 @@ def multiple():
             result.append(i)
         else:
             pass
-    print(result)
+    print(sum(result))
 
 
 if __name__ == '__main__':
